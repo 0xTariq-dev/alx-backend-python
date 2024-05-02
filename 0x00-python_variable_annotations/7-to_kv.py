8380d03164aee2b2fa3db9e2a609066960524a03
Tariq Abu-Elhamd <tariqmohsen222@gmail.com>
2024-05-02 22:41:53 +0300

    fix: edit the type of variable v in to_kv function

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -4,15 +4,17 @@
  the second element is the square of the int v and should be annotated
  as a float."""
 
-from typing import Tuple
+from typing import Tuple, Union
+
+num = Union[int, float]
 
 
-def to_kv(k: str, v: float) -> Tuple[str, float]:
+def to_kv(k: str, v: num) -> Tuple[str, float]:
     """
     Args:
         k: str
         v: float or int
     Returns:
         Tuple[str, float]: a tuple containing the string k and the square of v
-        """
+    """
     return (k, v ** 2)
