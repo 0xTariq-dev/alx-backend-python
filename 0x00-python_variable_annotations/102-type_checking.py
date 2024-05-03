f0aa971520394352eea4b1e4f897ee67de8f04ad
Tariq Abu-Elhamd <tariqmohsen222@gmail.com>
2024-05-03 19:38:10 +0300

    doc: zoom_array method documentation

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python3
-""""""
+"""This module contains a function zoom_array"""
 
-from typing import Tuple, List, Union
+from typing import Tuple, List
 
 
 def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """
+    Args:
+        lst (Tuple): a tuple of values of any type
+        factor (int, optional): a number by which the tuple will be zoomed.
+
+    Returns:
+        List: a list of values of any type
+    """
     zoomed_in: List = [
         item for item in lst
         for i in range(factor)
