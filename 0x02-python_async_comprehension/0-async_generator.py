985d2dcdfe09eb546bd44188c47964c4961fae63
Tariq Abu-Elhamd <tariqmohsen222@gmail.com>
2024-05-07 16:55:32 +0300

    feat: called async_generator The coroutine will loop 10 times, each time asynchronously wait 1 second, then yield a random number between 0 and 10. Use the random module

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""This module contains a coroutine called async_generator that takes no
+parameter and returns an async iterator."""
+
+import asyncio
+import random
+from typing import AsyncIterator, List
+
+
+async def async_generator() -> AsyncIterator[float]:
+    """The coroutine will loop 10 times, each time asynchronously
+    wait 1 second, then yield a random number between 0 and 10."""
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
---
+++ 0x02-python_async_comprehension/README.md
@@ -0,0 +1,11 @@
+# Python Async Comprehension Project
+
+## `Description`
+This is a simple project to demonstrate the use of Python's async comprehension.
+The project aims to deepen the understanding of async comprehension and its use cases.
+
+## `Dependencies`
+- Python 3.7 or higher
+- asyncio
+- random
+
